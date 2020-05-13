e1537270d3c6657e6b6dfef927756f5d018f375d
abhinav dixit <abhinavdixit2306@gmail.com>
2020-05-13 22:54:41 +0530

    react native component 1

--- App.tsx
+++ App.tsx
@@ -1,20 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>This is Abhinav Dixit</Text>
-    </View>
+  <Main />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'pink',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
